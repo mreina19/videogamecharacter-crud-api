93abf1afee04007d37b942b1fd5cc9c13183b304
Miguel <miguelnreina@proton.me>
2026-06-25 14:43:36 +0100

    Update AppDbContext comments

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,13 +7,6 @@ namespace VideoGameCharacter.Data
     //DbContext is the configuration object for the context.
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        /*  Old Constructor way (pre C# 12)
-            public AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
-            {
-                *Nothing here. Options are passed directly to DbContext base class.*
-            }
-        */
-
         //DbSet is EF Core's representation of a database table in C#.
         public DbSet<Character> Characters => Set<Character>();
     }
